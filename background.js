e61a96f0173a6cb5e070fd0172ccf9f5c4415349
Al Morris <paarsar@gmail.com>
2014-12-31 19:07:23 -0500

    test

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('window.html', {
-    'bounds': {
+    'bounds': {  
       'width': 400,
       'height': 500
     }
